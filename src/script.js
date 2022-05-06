723cf5e827d46136555dc62bd08d6f68c596bc1b
KotenkoNata <kotenkonata8@gmail.com>
2022-05-06 17:03:26 +0300

    fix: recreate keyLayout structure

--- src/script.js
+++ src/script.js
@@ -1,3 +1,396 @@
+const keyLayout = [
+  [
+    {
+      code: 'Backquote',
+      layouts: {
+        rus: 'ё',
+        en: '`',
+      },
+    },
+    {
+      code: 'Digit1',
+      layouts: {
+        rus: '!',
+        en: '1',
+      },
+    },
+    {
+      code: 'Digit2',
+      layouts: {
+        rus: '"',
+        en: '2',
+      },
+    },
+    {
+      code: 'Digit3',
+      layouts: {
+        rus: '№',
+        en: '3',
+      },
+    },
+    {
+      code: 'Digit4',
+      layouts: {
+        rus: ';',
+        en: '4',
+      },
+    }, {
+    code: 'Digit5',
+    layouts: {
+      rus: '%',
+      en: '5',
+    },
+  }, {
+    code: 'Digit6',
+    layouts: {
+      rus: ':',
+      en: '6',
+    },
+  }, {
+    code: 'Digit7',
+    layouts: {
+      rus: '?',
+      en: '7',
+    },
+  }, {
+    code: 'Digit8',
+    layouts: {
+      rus: '*',
+      en: '8',
+    },
+  }, {
+    code: 'Digit9',
+    layouts: {
+      rus: '(',
+      en: '9',
+    },
+  }, {
+    code: 'Digit0',
+    layouts: {
+      rus: ')',
+      en: '0',
+    },
+  }, {
+    code: 'Minus',
+    layouts: {
+      rus: '_',
+      en: '-',
+    },
+  }, {
+    code: 'Equal',
+    layouts: {
+      rus: '+',
+      en: '=',
+    },
+  }, {
+    code: 'Backspace',
+    layouts: {
+      rus: 'delete',
+      en: 'delete',
+    },
+  }],
+  [{
+    code: 'Tab',
+    layouts: {
+      rus: 'tab',
+      en: 'tab',
+    },
+  }, {
+    code: 'KeyQ',
+    layouts: {
+      rus: 'й',
+      en: 'q',
+    },
+  }, {
+    code: 'KeyW',
+    layouts: {
+      rus: 'ц',
+      en: 'w',
+    },
+  }, {
+    code: 'KeyE',
+    layouts: {
+      rus: 'у',
+      en: 'e',
+    },
+  }, {
+    code: 'KeyR',
+    layouts: {
+      rus: 'к',
+      en: 'r',
+    },
+  }, {
+    code: 'KeyT',
+    layouts: {
+      rus: 'е',
+      en: 't',
+    },
+  }, {
+    code: 'KeyY',
+    layouts: {
+      rus: 'н',
+      en: 'y',
+    },
+  }, {
+    code: 'KeyU',
+    layouts: {
+      rus: 'г',
+      en: 'u',
+    },
+  }, {
+    code: 'KeyI',
+    layouts: {
+      rus: 'ш',
+      en: 'i',
+    },
+  }, {
+    code: 'KeyO',
+    layouts: {
+      rus: 'щ',
+      en: 'o',
+    },
+  }, {
+    code: 'KeyP',
+    layouts: {
+      rus: 'з',
+      en: 'p',
+    },
+  }, {
+    code: 'BracketLeft',
+    layouts: {
+      rus: 'х',
+      en: '[',
+    },
+  }, {
+    code: 'BracketRight',
+    layouts: {
+      rus: 'ъ',
+      en: ']',
+    },
+  }, {
+    code: 'Backslash',
+    layouts: {
+      rus: '\\',
+      en: '\\',
+    },
+  }],
+  [{
+    code: 'CapsLock',
+    layouts: {
+      rus: 'caps lock',
+      en: 'caps lock',
+    },
+  }, {
+    code: 'KeyA',
+    layouts: {
+      rus: 'ф',
+      en: 'a',
+    },
+  }, {
+    code: 'KeyS',
+    layouts: {
+      rus: 'ы',
+      en: 's',
+    },
+  }, {
+    code: 'KeyD',
+    layouts: {
+      rus: 'в',
+      en: 'd',
+    },
+  }, {
+    code: 'KeyF',
+    layouts: {
+      rus: 'а',
+      en: 'f',
+    },
+  }, {
+    code: 'KeyG',
+    layouts: {
+      rus: 'п',
+      en: 'g',
+    },
+  }, {
+    code: 'KeyH',
+    layouts: {
+      rus: 'р',
+      en: 'h',
+    },
+  }, {
+    code: 'KeyJ',
+    layouts: {
+      rus: 'о',
+      en: 'j',
+    },
+  }, {
+    code: 'KeyK',
+    layouts: {
+      rus: 'л',
+      en: 'k',
+    },
+  }, {
+    code: 'KeyL',
+    layouts: {
+      rus: 'д',
+      en: 'l',
+    },
+  }, {
+    code: 'Semicolon',
+    layouts: {
+      rus: 'ж',
+      en: ';',
+    },
+  }, {
+    code: 'Quote',
+    layouts: {
+      rus: 'э',
+      en: '\'',
+    },
+  }, {
+    code: 'Enter',
+    layouts: {
+      rus: 'return',
+      en: 'return',
+    },
+  }],
+  [{
+    code: 'ShiftLeft',
+    layouts: {
+      rus: 'shift',
+      en: 'shift',
+    },
+  }, {
+    code: 'KeyZ',
+    layouts: {
+      rus: 'я',
+      en: 'z',
+    },
+  }, {
+    code: 'KeyX',
+    layouts: {
+      rus: 'ч',
+      en: 'x',
+    },
+  }, {
+    code: 'KeyC',
+    layouts: {
+      rus: 'с',
+      en: 'c',
+    },
+  }, {
+    code: 'KeyV',
+    layouts: {
+      rus: 'м',
+      en: 'v',
+    },
+  }, {
+    code: 'KeyB',
+    layouts: {
+      rus: 'и',
+      en: 'b',
+    },
+  }, {
+    code: 'KeyN',
+    layouts: {
+      rus: 'т',
+      en: 'n',
+    },
+  }, {
+    code: 'KeyM',
+    layouts: {
+      rus: 'ь',
+      en: 'M',
+    },
+  }, {
+    code: 'Comma',
+    layouts: {
+      rus: 'б',
+      en: ',',
+    },
+  }, {
+    code: 'Period',
+    layouts: {
+      rus: 'ю',
+      en: '.',
+    },
+  }, {
+    code: 'Slash',
+    layouts: {
+      rus: '.',
+      en: '/',
+    },
+  }, {
+    code: 'ShiftRight',
+    layouts: {
+      rus: 'shift',
+      en: 'shift',
+    },
+  }],
+  [{
+    code: 'ControlLeft',
+    layouts: {
+      rus: '⌃',
+      en: '⌃',
+    },
+  }, {
+    code: 'AltLeft',
+    layouts: {
+      rus: '⌥',
+      en: '⌥',
+    },
+  }, {
+    code: 'MetaLeft',
+    layouts: {
+      rus: '⌘',
+      en: '⌘',
+    },
+  }, {
+    code: 'Space',
+    layouts: {
+      rus: 'space',
+      en: 'space',
+    },
+  }, {
+    code: 'MetaRight',
+    layouts: {
+      rus: '⌘',
+      en: '⌘',
+    },
+  }, {
+    code: 'AltRight',
+    layouts: {
+      rus: '⌥',
+      en: '⌥',
+    },
+  }, {
+    code: 'ArrowUp',
+    layouts: {
+      rus: '▲',
+      en: '▲',
+    },
+  }, {
+    code: 'ArrowLeft',
+    layouts: {
+      rus: '◀',
+      en: '◀',
+    },
+  }, {
+    code: 'ArrowDown',
+    layouts: {
+      rus: '▼',
+      en: '▼',
+    },
+  }, {
+    code: 'ArrowRight',
+    layouts: {
+      rus: '▶',
+      en: '▶',
+    },
+  }],
+];
+
+console.log(keyLayout[0][0].layouts.en);
+
 const body = document.querySelector('#body');
 
 body.insertAdjacentHTML('beforeend', '<div class="container">'
@@ -222,22 +615,12 @@ body.insertAdjacentHTML('beforeend', '<div class="container">'
   + '<p>Switch between languages: command (⌘) + space</p>'
   + '</div>');
 
-document.addEventListener('keydown', (event) => {
-  console.log(event.code);
-  console.log(event.key);
-});
-
 const keyboard = {
   elements: {
     textarea: null,
     main: null,
     keysContainer: null,
     keys: [],
-  },
-
-  eventHandlers: {
-    oninput: null,
-    onclose: null,
   },
 
   properties: {
@@ -272,81 +655,77 @@ const keyboard = {
       + '<p>Keyboard was created for macOS</p>'
       + '<p>Switch between languages: command (⌘) + space</p>'
       + '</div>');
+
+    // Automatically use keyboard for elements with .input
+
+    document.querySelectorAll('.input').forEach((element) => {
+      element.addEventListener('focus', () => {
+        this.open(element.value, (currentValue) => {
+          element.value = currentValue;
+        });
+      });
+    });
   },
   createKeys() {
     const fragment = document.createDocumentFragment();
-    const keyLayoutEn = [
-      ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'delete'],
-      ['tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\'],
-      ['caps lock', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', '\'', 'return'],
-      ['shift', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shift'],
-      ['fn', '⌃', '⌥', '⌘', 'space', '⌘', '⌥', '▲', '◀', '▼', '▶'],
-    ];
 
-    keyLayoutEn.forEach((key) => {
+    keyLayout.forEach((firstArray) => {
       const ulElement = document.createElement('ul');
 
       ulElement.classList.add('rows');
 
-      key.forEach((element) => {
+      firstArray.forEach((element) => {
+console.log(element)
+
         const liElement = document.createElement('li');
-        const spanElement = document.createElement('span');
 
-        liElement.appendChild(spanElement);
-
-        if (element === '▲') {
+        if (element.layouts.en === '▲') {
           liElement.classList.add('top-arrow');
         }
-        if (element === '◀') {
+        if (element.layouts.en === '◀') {
           liElement.classList.add('left-arrow');
         }
-        if (element === '▼') {
+        if (element.layouts.en === '▼') {
           liElement.classList.add('bottom-arrow');
         }
-        if (element === '▶') {
+        if (element.layouts.en === '▶') {
           liElement.classList.add('right-arrow');
         }
 
-        switch (element) {
-          case 'backspace':
-            spanElement.addEventListener('click', () => {
-              this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
-              this._triggerEvent('oninput');
-            });
+        if (element.layouts.en.length > 1) {
+          liElement.classList.add('special');
+        }
 
-            break;
-
-          case 'caps lock':
-            liElement.addEventListener('click', () => {
-              this._toggleCapsLock();
-              liElement.classList.toggle('keyboard__key--active', this.properties.capsLock);
-            });
-
-            break;
+        switch (element.layouts.en) {
+          // case 'delete':
+          //   liElement.addEventListener('click', () => {
+          //     this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+          //   });
+          //
+          //   break;
 
           case 'enter':
-            spanElement.addEventListener('click', () => {
+            liElement.addEventListener('click', () => {
               this.properties.value += '\n';
-              this._triggerEvent('oninput');
             });
 
             break;
 
           case 'space':
-            spanElement.classList.add('space');
+            liElement.classList.add('space');
 
-            spanElement.addEventListener('click', () => {
+            liElement.addEventListener('click', () => {
               this.properties.value += ' ';
-              this._triggerEvent('oninput');
             });
 
             break;
 
           default:
-            spanElement.textContent = element.toLowerCase();
-            spanElement.addEventListener('click', () => {
-              this.properties.value += this.properties.capsLock ? element.toUpperCase() : element.toLowerCase();
-              this._triggerEvent('oninput');
+            liElement.textContent = element.layouts.en.toLowerCase();
+
+            liElement.classList.add(`${element.code}`);
+            liElement.addEventListener('click', () => {
+              this.properties.value += this.properties.capsLock ? element.layouts.en.toUpperCase() : element.layouts.en.toLowerCase();
             });
 
             break;
@@ -359,30 +738,44 @@ const keyboard = {
     return fragment;
   },
 
-  _triggerEvent(handlerName) {
-    if (typeof this.eventHandlers[handlerName] === 'function') {
-      this.eventHandlers[handlerName](this.properties.value);
-    }
-  },
-
-  _toggleCapsLock: function () {
+  _toggleCapsLock() {
     this.properties.capsLock = !this.properties.capsLock;
     for (const key of this.elements.keys) {
-      key.textContent = this.properties.capsLock ? key.textContent.toUpperCase()
-        : key.textContent.toLowerCase();
+      if (!key.classList.contains('special')) {
+        key.textContent = this.properties.capsLock ? key.textContent.toUpperCase()
+          : key.textContent.toLowerCase();
+      }
     }
   },
 
-  open(initial, oninput, onclose) {
+  keyBehavior() {
+    const capsLockElement = document.querySelector('.CapsLock');
+    capsLockElement.addEventListener('click', () => {
+      this._toggleCapsLock();
+      capsLockElement.classList.toggle('capsLock-active', this.properties.capsLock);
+    });
 
+    for (const key of this.elements.keys) {
+      key.addEventListener('click', (event) => {
+        console.log('event.code', event.code, 'event.key', event.key);
+      });
+    }
   },
 
-  close() {
-
+  handleKeyPress() {
+    document.addEventListener('keypress', (event) => {
+      const array = this.elements.keys;
+      array.forEach((key) => {
+        if (key.textContent === event.key) {
+          key.classList.add('active');
+        }
+      });
+    });
   },
-
 };
 
 window.addEventListener('DOMContentLoaded', () => {
   keyboard.init();
+  keyboard.keyBehavior();
+  keyboard.handleKeyPress();
 });
